1c90a21126cfe5e61b721a41f383831c387f8638
Gauri Dhakad <gauri@Gauris-Laptop.local>
2025-06-29 14:45:06 +0530

    feat/homepage-style:HomepageCompleted

--- src/app/reusable components/newevent/newevent.ts
+++ src/app/reusable components/newevent/newevent.ts
@@ -1,11 +1,18 @@
+import { TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import{Input} from '@angular/core'
+import { Input } from '@angular/core';
 @Component({
   selector: 'app-newevent',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './newevent.html',
-  styleUrl: './newevent.scss'
+  styleUrl: './newevent.scss',
 })
 export class Newevent {
-  @Input() newdetails={id:1,label:"workshop",eventName:"hi",dateTime:"djdndj",imageLink:"pands.jpeg"};
+  @Input() newdetails = {
+    id: 1,
+    label: 'workshop',
+    eventName: 'hi',
+    dateTime: 'djdndj',
+    imageLink: 'pands.jpeg',
+  };
 }
