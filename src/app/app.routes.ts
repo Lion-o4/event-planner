43c3a105393de9f7c165224baca72a1e99fe6ea8
Gauri Dhakad <gauri@Gauris-Laptop.local>
2025-07-22 03:02:27 +0530

    feat(feedback):added schedulepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomePage } from './Components/home-page/home-page';
 import { About } from './Components/about/about';
 import { Contact } from './Components/contact/contact';
+import { Schedule } from './Components/schedule/schedule';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePage },
   { path: 'about-me', component: About },
   { path: 'Bucket-List', component: Contact },
+  { path: 'schedule', component: Schedule },
 ];
